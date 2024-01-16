7296f8587712cf1db4847cd202c73613b92a0959
joharikushagra <skjkushagra1309@gmail.com>
2024-01-16 15:19:55 +0530

    style fix

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,7 +4,7 @@ import ListItem from "./ListItem";
 const List = ({userList,addChip}) => {
   return (
     <div className="absolute mt-3">
-     <ul className="bg-white rounded-md max-h-[360px] overflow-y-scroll">
+     <ul className="bg-white max-h-[360px] overflow-y-scroll shadow-md rounded-lg shadow-zinc-200	">
        {userList.map((u,i)=><ListItem key={i} user={u} idx={i} addChip={addChip}/>)}
      </ul>
     </div>
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ListItem = ({user,idx,addChip}) => {
   return (
-    <li className="flex justify-between gap-x-6 py-2 border-b-2 border-neutral-300" onClick={()=>addChip(idx)}>
+    <li className="flex justify-between gap-x-6 py-2 border-b-2 border-neutral-100" onClick={()=>addChip(idx)}>
       <div className="flex min-w-0 gap-x-4 px-2">
         <img
           className="h-12 w-12 flex-none rounded-full bg-gray-50"
